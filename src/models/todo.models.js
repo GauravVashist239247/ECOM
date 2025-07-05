1dd5cbf91c6909ee6fef4392b376521700535efa
GauravVashist239247 <gvashist1010@gmail.com>
2025-07-05 13:15:54 +0530

    todo delete model

--- src/models/todo.models.js
+++ src/models/todo.models.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const TodoSchema = new mongoose.Schema({
+  name: { type: String, require: true },
   task: { type: String, require: true },
   duedate: {
     type: Date,

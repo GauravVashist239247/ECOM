211aa3186a04e8750bd3456ad9fc8e8ce60f9f10
GauravVashist239247 <gvashist1010@gmail.com>
2025-08-15 15:51:09 +0530

    logout 2 n second

--- src/controller/authTodo/logoutUser.controller.js
+++ src/controller/authTodo/logoutUser.controller.js
@@ -1,11 +1,6 @@
 const logout = (req, res) => {
   try {
-    res.cookie("token", {
-      httpOnly: true,
-      sameSite: "None",
-      secure: true,
-    });
-
+    res.clearCookie("token");
     return res.status(200).json({ message: "Logout successful" });
   } catch (err) {
     console.error("Logout error:", err);
